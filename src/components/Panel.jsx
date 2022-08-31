3fcd9c90bcbca187e84a8d9a2200cf90d0ab850b
gguesnet <guillaume@macbook-pro-de-guillaume.local>
2022-08-31 00:59:00 +0000

    Découpage Transaction + Props dans Panel

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,3 +1,5 @@
+import Transaction from "./Transaction";
+
 function Panel() {
   return (
     <main class="main bg-dark">
@@ -10,36 +12,21 @@ function Panel() {
         <button class="edit-button">Edit Name</button>
       </div>
       <h2 class="sr-only">Accounts</h2>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Checking (x8349)</h3>
-          <p class="account-amount">$2,082.79</p>
-          <p class="account-amount-description">Available Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Savings (x6712)</h3>
-          <p class="account-amount">$10,928.42</p>
-          <p class="account-amount-description">Available Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
-      <section class="account">
-        <div class="account-content-wrapper">
-          <h3 class="account-title">Argent Bank Credit Card (x8349)</h3>
-          <p class="account-amount">$184.30</p>
-          <p class="account-amount-description">Current Balance</p>
-        </div>
-        <div class="account-content-wrapper cta">
-          <button class="transaction-button">View transactions</button>
-        </div>
-      </section>
+      <Transaction
+        title="Argent Bank Checking (x8349)"
+        amount="$2,082.79"
+        description="Available Balance"
+      />
+      <Transaction
+        title="Argent Bank Savings (x6712)"
+        amount="$10,928.42"
+        description="Available Balance"
+      />
+      <Transaction
+        title="Argent Bank Checking (x8349)"
+        amount="$184.30"
+        description="Current Balance"
+      />
     </main>
   );
 }
