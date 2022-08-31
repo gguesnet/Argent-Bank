9409fc3e290d02f8d2971fe6677f32b5260467f7
gguesnet <guillaume@macbook-pro-de-guillaume.local>
2022-08-31 01:58:59 +0000

    Fix class to className in JSX Panel + Transaction

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,16 +1,24 @@
+import PropTypes from "prop-types";
+
 function Transaction({ title, amount, description }) {
   return (
-    <section class="account">
-      <div class="account-content-wrapper">
-        <h3 class="account-title">{title}</h3>
-        <p class="account-amount">{amount}</p>
-        <p class="account-amount-description">{description}</p>
+    <section className="account">
+      <div className="account-content-wrapper">
+        <h3 className="account-title">{title}</h3>
+        <p className="account-amount">{amount}</p>
+        <p className="account-amount-description">{description}</p>
       </div>
-      <div class="account-content-wrapper cta">
-        <button class="transaction-button">View transactions</button>
+      <div className="account-content-wrapper cta">
+        <button className="transaction-button">View transactions</button>
       </div>
     </section>
   );
 }
+
+Transaction.prototype = {
+  title: PropTypes.string.isRequired,
+  amount: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
 export default Transaction;
